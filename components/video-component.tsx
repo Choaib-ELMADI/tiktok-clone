2e0cb362d80e70ead59c8d4290b98e73b05fcb9f
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-08-04 22:58:12 +0100

    Some styles for the video

--- components/video-component.tsx
+++ components/video-component.tsx
@@ -11,8 +11,8 @@ interface VideoProps {
 
 const Video = ({ video }: VideoProps) => {
 	return (
-		<div className="pb-4 border-b border-border max-w-[700px] w-full overflow-hidden">
-			<div className="flex gap-2 mb-3">
+		<div className="pb-4 border-b border-border max-w-[700px] w-full">
+			<div className="flex gap-2">
 				<Link href={`@${video.userLink}`}>
 					<Avatar className="bg-text text-background w-[52px] h-[52px]">
 						<AvatarImage src={video.userProfileImageUrl} />
@@ -25,45 +25,47 @@ const Video = ({ video }: VideoProps) => {
 				<div className="flex flex-col">
 					<Link
 						href={`@${video.userLink}`}
-						className="hover:underline font-semibold"
+						className="hover:underline font-semibold truncate w-[140px]"
 					>
-						{video.userLink}
+						{video.userLink}blablabla
 					</Link>
-					<h3 className="text-sm w-max rounded-full py-[2px] px-[6px] bg-light_white text-light_gray dark:text-light_white dark:bg-light_gray">
+					<h3 className="text-[.8rem] w-max rounded-full py-[2px] px-[6px] bg-light_white text-light_gray dark:text-light_white dark:bg-light_gray">
 						{video.userName}
 					</h3>
-					<p className="text-sm my-[3px]">
-						{video.caption}{" "}
-						{video.hashtags
-							.split("#")
-							.filter((i) => i !== "")
-							.map((hash, i) => (
-								<Link
-									href={`/tags/${hash}`}
-									key={`hash-${i}`}
-									className="text-blue-500 dark:text-brand_2 hover:underline ml-[4px] font-semibold"
-								>
-									#{hash}
-								</Link>
-							))}
-					</p>
-					<div className="flex items-center text-sm gap-[4px]">
-						<Music className="w-[14px] h-[14px]" />
-						<span>original sound - </span>
-						<span className="text-[.8rem]">{video.userName}</span>
-					</div>
 				</div>
 				<Button variant="outline" size="lg" className="ml-auto">
 					Follow
 				</Button>
 			</div>
-			<div className="flex items-end gap-4">
+			<div className="ml-[60px] mb-2">
+				<p className="text-sm my-[3px]">
+					{video.caption}{" "}
+					{video.hashtags
+						.split("#")
+						.filter((i) => i !== "")
+						.map((hash, i) => (
+							<Link
+								href={`/tags/${hash}`}
+								key={`hash-${i}`}
+								className="text-blue-500 dark:text-brand_2 hover:underline ml-[4px] font-semibold"
+							>
+								#{hash}
+							</Link>
+						))}
+				</p>
+				<div className="flex items-center text-sm gap-[4px]">
+					<Music className="w-[14px] h-[14px]" />
+					<span>original sound - </span>
+					<span className="text-[.8rem]">{video.userName}</span>
+				</div>
+			</div>
+			<div className="flex items-end gap-4 w-full">
 				<video
 					controls
 					disablePictureInPicture
 					loop
 					controlsList="nodownload noplaybackrate"
-					className="h-[460px] xs:h-[540px] ml-[60px] rounded-md hide-controls"
+					className="h-[460px] xs:h-[540px] max-w-[calc(100%-122px)] w-max ml-[60px] rounded-md hide-controls bg-light_white dark:bg-light_gray"
 				>
 					<source src={video.source} />
 				</video>
