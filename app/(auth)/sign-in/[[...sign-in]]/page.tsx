a7032305071aed5becdcd821cc24708b515c10d6
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-08-05 00:26:07 +0100

    Add some paddings to sign in / sign up

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
 	return (
-		<div className="flex justify-center items-center h-screen">
+		<div className="flex justify-center items-center min-h-screen px-4 py-8">
 			<SignIn />
 		</div>
 	);
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
 	return (
-		<div className="flex items-center justify-center h-screen">
+		<div className="flex items-center justify-center min-h-screen px-4 py-8">
 			<SignUp />
 		</div>
 	);
