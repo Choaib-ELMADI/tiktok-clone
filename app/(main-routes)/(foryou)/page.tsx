ed64dd141a70834349c608cddacd6f0816f7dab6
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-08-11 23:01:38 +0100

    Add the video skeleton

--- app/(main-routes)/(foryou)/page.tsx
+++ app/(main-routes)/(foryou)/page.tsx
@@ -8,7 +8,17 @@ export default async function Home() {
 
 	return (
 		<div className="flex flex-col items-center gap-8 p-4 pb-0">
-			{videos.length === 0 && <div>Skeleton</div>}
+			{videos.length === 0 && (
+				<div className="w-full max-w-[700px]">
+					<div className="flex gap-2">
+						<div className="bg-light_white dark:bg-light_gray w-[52px] h-[52px] rounded-full" />
+						<div className="flex flex-col gap-2">
+							<div className="w-[140px] bg-light_white dark:bg-light_gray h-5 rounded-lg" />
+							<div className="w-[200px] bg-light_white dark:bg-light_gray h-5 rounded-lg" />
+						</div>
+					</div>
+				</div>
+			)}
 			{videos.map((video) => (
 				<Video key={video.id} video={video} />
 			))}
