96622f5389aa6146a790f22b428d57ce1f662763
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-08-12 15:58:14 +0100

    Add an error page

--- app/(special-routes)/error/page.tsx
+++
@@ -1,7 +0,0 @@
-const ErrorPage = () => {
-	return (
-		<div>
-			<h1>An error occured</h1>
-		</div>
-	);
-};
---
+++ app/error.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+import Footer from "@/components/footer";
+
+export default function ErrorPage({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	return (
+		<main className="pt-[60px] flex flex-col bg-white text-black">
+			<div className="w-full flex flex-col justify-center items-center min-h-[400px]">
+				<h1 className="text-2xl font-semibold">An error occured</h1>
+				<p className="text-xl font-medium text-red-500 mb-8">{error.message}</p>
+				<div className="flex gap-4">
+					<Button variant="outline" size="lg" onClick={() => reset()}>
+						Try again
+					</Button>
+					<Button
+						variant="default"
+						size="lg"
+						className="text-white hover:text-black"
+					>
+						<Link href="/">ForYou Page</Link>
+					</Button>
+				</div>
+			</div>
+			<Footer />
+		</main>
+	);
+}
