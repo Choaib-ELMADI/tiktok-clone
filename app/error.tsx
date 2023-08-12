dc741e990859e65a33ab639111e151be9a076b81
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-08-12 18:21:10 +0100

    Some styles

--- app/error.tsx
+++ app/error.tsx
@@ -16,7 +16,7 @@ export default function ErrorPage({
 		<main className="pt-[60px] flex flex-col bg-white text-black">
 			<div className="w-full flex flex-col justify-center items-center min-h-[400px]">
 				<h1 className="text-2xl font-semibold">An error occured</h1>
-				<p className="text-xl font-medium text-red-500 w-full max-x-[500px] text-center mb-8">
+				<p className="text-xl font-medium text-red-500 w-full max-w-[500px] text-center mb-8">
 					{error.message}
 				</p>
 				<div className="flex gap-4">
